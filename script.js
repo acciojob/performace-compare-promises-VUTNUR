8b4ceca3d36cc0a4b27168f8e1bf55f6eaf0916d
Vutnur Nethaji Goud <130832771+VUTNUR@users.noreply.github.com>
2023-05-29 19:12:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,3 +13,28 @@ const apiUrls = [
 ];
 
 // You can write your code here
+function fetchUrl(url){
+  let response=fetch(url);
+  let response1= response.then((data)=>{
+    data.json.then((info)=>{
+      return info
+    })
+  })
+  return response1;
+}
+function displayTimeTaken(method, timeTaken) {
+  const outputElement = document.getElementById(`output-${method}`);
+  outputElement.textContent = timeTaken;
+}
+const startTimeAll= performance.now();
+Promise.all(apiUrls.map(fetchUrl)).then(()=>{
+  const endTimeAll=performance.now();
+  const diff= endTimeAll-startTimeAll;
+   displayTimeTaken(all, diff)
+})
+const startTimeAny=performance.now();
+Promise.any(apiUrls.map(fetchUrl)).then(()=>{
+  const endTimeAny=performance.now();
+  const diff2= endTimeAny-startTimeAny;
+  displayTimeTaken(any, diff2)
+})
